8d6059fbdf4ad29d3c49ed6275a9c5edb721d883
Jochen Hassfurter <jo@hassfurter.com>
2018-05-10 19:28:31 +0200

    MacOS X

--- src/main/java/rocks/gebsattel/hochzeit/config/security/SecurityConfiguration.java
+++ src/main/java/rocks/gebsattel/hochzeit/config/security/SecurityConfiguration.java
@@ -59,7 +59,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
                 .defaultSuccessUrl("/admin/home")
-                .usernameParameter("lastName")
+                .usernameParameter("email")
                 .passwordParameter("password")
                 .and().logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
